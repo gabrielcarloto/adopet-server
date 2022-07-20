f35884d9fbf003995d90bdfd6ad539a4be046010
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2022-07-19 21:16:53 -0300

    ✨ feat: add server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-// Hello World!
+import { ApolloServer } from 'apollo-server';
+import resolvers from './resolvers';
+import typeDefs from './schema';
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  csrfPrevention: true,
+  cache: 'bounded',
+});
+
+server.listen().then(({ url }) => {
+  // eslint-disable-next-line no-console
+  console.log(`🚀  Server ready at ${url}`);
+});
